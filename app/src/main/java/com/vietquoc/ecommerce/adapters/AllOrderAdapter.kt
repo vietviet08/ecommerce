bf83883d1dfa840478d768cfa816efef971906c5
vietviet08 <vietnq.23ceb@vku.udn.vn>
2025-01-20 00:04:04 +0700

    show all orders in orders fragment

---
+++ app/src/main/java/com/vietquoc/ecommerce/adapters/AllOrderAdapter.kt
@@ -0,0 +1,82 @@
+package com.vietquoc.ecommerce.adapters
+
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.vietquoc.ecommerce.R
+import com.vietquoc.ecommerce.data.order.Order
+import com.vietquoc.ecommerce.data.order.OrderStatus
+import com.vietquoc.ecommerce.data.order.getOrderStatus
+import com.vietquoc.ecommerce.databinding.OrderItemBinding
+
+class AllOrderAdapter : RecyclerView.Adapter<AllOrderAdapter.AllOrderViewHolder>() {
+
+    inner class AllOrderViewHolder(val binding: OrderItemBinding) : ViewHolder(binding.root) {
+        fun bind(order: Order) {
+            binding.apply {
+                tvOrderId.text = order.orderId.toString()
+                tvOrderDate.text = order.date
+                val resources = itemView.resources
+                val colorDrawable = when (getOrderStatus(order.orderStatus)) {
+                    is OrderStatus.Ordered -> {
+                        ColorDrawable(resources.getColor(R.color.g_orange_yellow))
+                    }
+                    is OrderStatus.Confirmed -> {
+                        ColorDrawable(resources.getColor(R.color.g_green))
+                    }
+                    is OrderStatus.Delivered -> {
+                        ColorDrawable(resources.getColor(R.color.g_green))
+                    }
+                    is OrderStatus.Shipped -> {
+                        ColorDrawable(resources.getColor(R.color.g_green))
+                    }
+                    is OrderStatus.Canceled -> {
+                        ColorDrawable(resources.getColor(R.color.g_red))
+                    }
+                    is OrderStatus.Returned -> {
+                        ColorDrawable(resources.getColor(R.color.g_red))
+                    }
+                }
+                imageOrderState.setImageDrawable(colorDrawable)
+            }
+        }
+
+    }
+
+    private val differCallback = object : DiffUtil.ItemCallback<Order>() {
+        override fun areItemsTheSame(p0: Order, p1: Order): Boolean {
+            return p0.products == p1.products
+        }
+
+        override fun areContentsTheSame(p0: Order, p1: Order): Boolean {
+            return p0 == p1
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): AllOrderViewHolder {
+        return AllOrderViewHolder(
+            OrderItemBinding.inflate(
+                LayoutInflater.from(p0.context),
+                p0,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onBindViewHolder(p0: AllOrderViewHolder, p1: Int) {
+        val order = differ.currentList[p1]
+        p0.bind(order)
+    }
+
+    var onClick: ((Order) -> Unit)? = null
+}
--- app/src/main/java/com/vietquoc/ecommerce/data/order/Order.kt
+++ app/src/main/java/com/vietquoc/ecommerce/data/order/Order.kt
@@ -1,14 +1,22 @@
 package com.vietquoc.ecommerce.data.order
 
-import com.google.firebase.Timestamp
+import android.os.Parcelable
 import com.vietquoc.ecommerce.data.Address
 import com.vietquoc.ecommerce.data.CartProduct
+import kotlinx.parcelize.Parcelize
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import kotlin.random.Random
 
-class Order(
+@Parcelize
+data class Order(
     val orderStatus: String = "",
     val totalPrice: Float = 0f,
     val products: List<CartProduct> = emptyList(),
     val address: Address = Address(),
-    val date: String = Timestamp.now().toDate().toString(),
-) {
+    val date: String = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault()).format(Date()),
+    val orderId: Long = Random.nextLong(0, 100_000_000_000) + totalPrice.toLong()
+) : Parcelable {
+    constructor() : this("", 0f, emptyList())
 }
